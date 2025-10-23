aef6d3353cabacfb654f688f645f009925df7b2f
ankitkumarmohanty3 <ankitkumarmohanty0@gmail.com>
2025-10-23 14:43:53 +0530

    updated vite config json

--- educational-portal/vite.config.js
+++ educational-portal/vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+   base: '/<edusity-react>/',
+   build: {
+    rollupOptions: {
+      external: [
+        // Replace 'unhandled-module-name' with the actual package name causing the error.
+        // You'll need to identify it from the surrounding build logs.
+        'unhandled-module-name' 
+      ],
+    },
+  },
 })
